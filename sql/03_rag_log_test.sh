ae0364e152526df2fe181b80544a6a0b8727c065
watanabeyasuaki <watanabeyasuaki@itsc-0027.local>
2021-03-03 16:32:44 +0900

    refs CloudRun用にDockerファイルをチューニングしたもの

--- sql/03_rag_log_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "CREATE DATABASE IF NOT EXISTS \`rag_log_test\` ;" | "${mysql[@]}"
-echo "GRANT ALL ON \`rag_log_test\`.* TO '"$MYSQL_USER"'@'%' ;" | "${mysql[@]}"
-echo 'FLUSH PRIVILEGES ;' | "${mysql[@]}"
